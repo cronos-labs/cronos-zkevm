b60e653892051355617c0cb229a86d044c0f74c5
jayt106 <jay.tseng@crypto.com>
2023-06-14 17:12:14 -0400

    fix implementation error

--- core/lib/object_store/src/s3.rs
+++ core/lib/object_store/src/s3.rs
@@ -2,66 +2,24 @@
 
 use aws_config::meta::region::RegionProviderChain;
 use aws_sdk_s3::{
-    config::{self, Region},
-    Client,
+    config::Region,
+    error::SdkError,
+    operation::{
+        delete_object::DeleteObjectError, get_object::GetObjectError, put_object::PutObjectError,
+    },
+    primitives::{ByteStream, ByteStreamError},
+    Client, Error,
 };
 use aws_types::SdkConfig;
-use s3_service::error::Error;
-use uuid::Uuid;
 
 use tokio::runtime::{Handle, RuntimeFlavor};
 
-use std::{fs::File, io::Write, path::PathBuf, process::exit};
-use tokio_stream::StreamExt;
-
-use std::{
-    fmt,
-    future::Future,
-    thread,
-    time::{Duration, Instant},
-};
-
-use clap::Parser;
-use tracing::log::trace;
-
-#[derive(Debug, Parser)]
-struct Opt {
-    #[structopt(long)]
-    bucket: String,
-    #[structopt(long)]
-    object: String,
-    #[structopt(long)]
-    destination: PathBuf,
-}
+use std::{fmt, future::Future, thread, time::Instant};
 
 use crate::raw::{Bucket, ObjectStore, ObjectStoreError};
 
-async fn retry<T, E, Fut, F>(max_retries: u16, mut f: F) -> Result<T, E>
-where
-    Fut: Future<Output = Result<T, E>>,
-    F: FnMut() -> Fut,
-{
-    let mut retries = 1;
-    let mut backoff = 1;
-    loop {
-        match f().await {
-            Ok(result) => return Ok(result),
-            Err(err) => {
-                vlog::warn!("Failed s3 request {retries}/{max_retries}, retrying.");
-                if retries > max_retries {
-                    return Err(err);
-                }
-                retries += 1;
-                tokio::time::sleep(Duration::from_secs(backoff)).await;
-                backoff *= 2;
-            }
-        }
-    }
-}
-
 pub struct AsyncS3Storage {
     bucket_prefix: String,
-    max_retries: u16,
     client: Client,
 }
 
@@ -70,42 +28,31 @@ impl fmt::Debug for AsyncS3Storage {
         formatter
             .debug_struct("S3StorageAsync")
             .field("bucket_prefix", &self.bucket_prefix)
-            .field("max_retries", &self.max_retries)
             .finish()
     }
 }
 
 impl AsyncS3Storage {
-    pub async fn new(
-        region: String,
-        credential_file_path: Option<String>,
-        bucket_prefix: String,
-        max_retries: u16,
-    ) -> Self {
-        let client_config = retry(max_retries, || {
-            Self::get_client_config(region, credential_file_path.clone())
-        })
-        .await
-        .expect("failed fetching s3 client config after retries");
+    pub async fn new(credential_file_path: Option<String>, bucket_prefix: String) -> Self {
+        let client_config = Self::get_client_config(credential_file_path.clone())
+            .await
+            .expect("failed fetching s3 client config after retries");
 
         Self {
-            client: Client::new(client_config),
+            client: Client::new(&client_config),
             bucket_prefix,
-            max_retries,
         }
     }
 
-    async fn get_client_config(
-        region: String,
-        credential_file_path: Option<String>,
-    ) -> Result<SdkConfig, Error> {
+    async fn get_client_config(_credential_file_path: Option<String>) -> Result<SdkConfig, Error> {
+        let region = "";
         let provider = RegionProviderChain::first_try(Region::new(region)).or_default_provider();
-        let provider_region = provider.region().await.unwrap();
+        let _provider_region = provider.region().await.unwrap();
 
         //TODO:
         //load credential_file_path as the account key and set to the client connection.
 
-        aws_config::from_env().region(provider).load().await
+        Ok(aws_config::from_env().region(provider).load().await)
     }
 
     fn filename(bucket: &str, filename: &str) -> String {
@@ -120,31 +67,36 @@ impl AsyncS3Storage {
         let started_at = Instant::now();
         let filename = Self::filename(bucket, key);
         vlog::trace!(
-            "Fetching data from GCS for key {filename} from bucket {}",
+            "Fetching data from S3 for key {filename} from bucket {}",
             self.bucket_prefix
         );
 
-        let request = GetObjectRequest {
-            bucket: self.bucket_prefix.clone(),
-            object: filename,
-            ..GetObjectRequest::default()
-        };
-        let range = Range::default();
-        let blob = retry(self.max_retries, || {
-            self.client.download_object(&request, &range)
-        })
-        .await;
+        let object = self
+            .client
+            .get_object()
+            .bucket(self.bucket_prefix.clone())
+            .key(filename.clone())
+            .send()
+            .await?;
 
         vlog::trace!(
-            "Fetched data from GCS for key {key} from bucket {bucket} and it took: {:?}",
+            "Fetched data from S3 for key {filename} from bucket {} and it took: {:?}",
+            self.bucket_prefix,
             started_at.elapsed()
         );
+
         metrics::histogram!(
             "server.object_store.fetching_time",
             started_at.elapsed(),
             "bucket" => bucket
         );
-        blob.map_err(ObjectStoreError::from)
+
+        object
+            .body
+            .collect()
+            .await
+            .map(|data| data.to_vec())
+            .map_err(ObjectStoreError::from)
     }
 
     pub(crate) async fn put_async(
@@ -156,23 +108,26 @@ impl AsyncS3Storage {
         let started_at = Instant::now();
         let filename = Self::filename(bucket, key);
         vlog::trace!(
-            "Storing data to GCS for key {filename} from bucket {}",
+            "Storing data to s3 for key {filename} from bucket {}",
             self.bucket_prefix
         );
 
-        let upload_type = UploadType::Simple(Media::new(filename));
-        let request = UploadObjectRequest {
-            bucket: self.bucket_prefix.clone(),
-            ..Default::default()
-        };
-        let object = retry(self.max_retries, || {
-            self.client
-                .upload_object(&request, value.clone(), &upload_type)
-        })
-        .await;
+        let body = ByteStream::from(value);
+
+        let result = self
+            .client
+            .put_object()
+            .bucket(self.bucket_prefix.clone())
+            .key(filename.clone())
+            .body(body)
+            .send()
+            .await?;
+
+        vlog::trace!("PutObjectOutput {:?}", result);
 
         vlog::trace!(
-            "Stored data to GCS for key {key} from bucket {bucket} and it took: {:?}",
+            "Stored data to S3 for key {filename} from bucket {} and it took: {:?}",
+            self.bucket_prefix,
             started_at.elapsed()
         );
         metrics::histogram!(
@@ -180,7 +135,7 @@ impl AsyncS3Storage {
             started_at.elapsed(),
             "bucket" => bucket
         );
-        object.map(drop).map_err(ObjectStoreError::from)
+        Ok(())
     }
 
     // For some bizzare reason, `async fn` doesn't work here, failing with the following error:
@@ -194,38 +149,60 @@ impl AsyncS3Storage {
     ) -> impl Future<Output = Result<(), ObjectStoreError>> + '_ {
         let filename = Self::filename(bucket, key);
         vlog::trace!(
-            "Removing data from GCS for key {filename} from bucket {}",
+            "Removing data from S3 for key {filename} from bucket {}",
             self.bucket_prefix
         );
 
-        let request = DeleteObjectRequest {
-            bucket: self.bucket_prefix.clone(),
-            object: filename,
-            ..DeleteObjectRequest::default()
-        };
+        let request = self
+            .client
+            .delete_object()
+            .bucket(self.bucket_prefix.clone())
+            .key(filename.clone());
+
         async move {
-            retry(self.max_retries, || self.client.delete_object(&request))
-                .await
-                .map_err(ObjectStoreError::from)
+            match request.send().await {
+                Ok(_s) => Ok(()),
+                Err(error) => return Err(ObjectStoreError::Other(error.into())),
+            }
         }
     }
 }
 
-impl From<HttpError> for ObjectStoreError {
-    fn from(err: HttpError) -> Self {
-        let is_not_found = match &err {
-            HttpError::HttpClient(err) => err
-                .status()
-                .map_or(false, |status| matches!(status, StatusCode::NOT_FOUND)),
-            HttpError::Response(response) => response.code == StatusCode::NOT_FOUND.as_u16(),
-            HttpError::TokenSource(_) => false,
-        };
-
-        if is_not_found {
-            ObjectStoreError::KeyNotFound(err.into())
-        } else {
-            ObjectStoreError::Other(err.into())
+impl From<SdkError<GetObjectError>> for ObjectStoreError {
+    fn from(sdk_err: SdkError<GetObjectError>) -> Self {
+        let err = sdk_err.into_service_error();
+        match err {
+            GetObjectError::InvalidObjectState(_) => ObjectStoreError::KeyNotFound(err.into()),
+            GetObjectError::NoSuchKey(_) => ObjectStoreError::KeyNotFound(err.into()),
+            GetObjectError::Unhandled(_) => ObjectStoreError::Other(err.into()),
+            _ => todo!(),
         }
+    }
+}
+
+impl From<SdkError<PutObjectError>> for ObjectStoreError {
+    fn from(sdk_err: SdkError<PutObjectError>) -> Self {
+        let err = sdk_err.into_service_error();
+        match err {
+            PutObjectError::Unhandled(_) => ObjectStoreError::Other(err.into()),
+            _ => todo!(),
+        }
+    }
+}
+
+impl From<SdkError<DeleteObjectError>> for ObjectStoreError {
+    fn from(sdk_err: SdkError<DeleteObjectError>) -> Self {
+        let err = sdk_err.into_service_error();
+        match err {
+            DeleteObjectError::Unhandled(_) => ObjectStoreError::Other(err.into()),
+            _ => todo!(),
+        }
+    }
+}
+
+impl From<ByteStreamError> for ObjectStoreError {
+    fn from(byte_stream_err: ByteStreamError) -> Self {
+        ObjectStoreError::Other(byte_stream_err.into())
     }
 }
 
@@ -239,7 +216,7 @@ impl S3Storage {
     pub fn new(
         credential_file_path: Option<String>,
         bucket_prefix: String,
-        max_retries: u16,
+        _max_retries: u16,
     ) -> Self {
         let handle = Handle::try_current().unwrap_or_else(|_| {
             panic!(
@@ -248,7 +225,10 @@ impl S3Storage {
                  run with `tokio::task::spawn_blocking()`."
             );
         });
-        let inner = AsyncS3Storage::new(credential_file_path, bucket_prefix, max_retries);
+
+        // TODO: S3 client has default 2 retries. we can pass max_retries to the s3 retry config
+
+        let inner = AsyncS3Storage::new(credential_file_path, bucket_prefix);
         Self {
             inner: Self::block_on(&handle, inner),
             handle,
@@ -290,7 +270,6 @@ impl ObjectStore for S3Storage {
 
 #[cfg(test)]
 mod test {
-    use std::sync::atomic::{AtomicU16, Ordering};
 
     use super::*;
 
@@ -312,38 +291,5 @@ mod test {
     #[tokio::test(flavor = "multi_thread", worker_threads = 4)]
     async fn blocking_in_sync_and_async_context_in_multithreaded_rt() {
         test_blocking().await;
-    }
-
-    #[tokio::test]
-    async fn test_retry_success_immediate() {
-        let result = retry(2, || async { Ok::<_, ()>(42) }).await;
-        assert_eq!(result, Ok(42));
-    }
-
-    #[tokio::test]
-    async fn test_retry_failure_exhausted() {
-        let result = retry(2, || async { Err::<i32, _>(()) }).await;
-        assert_eq!(result, Err(()));
-    }
-
-    async fn retry_success_after_n_retries(n: u16) -> Result<u32, String> {
-        let retries = AtomicU16::new(0);
-        let result = retry(n, || async {
-            let retries = retries.fetch_add(1, Ordering::Relaxed);
-            if retries + 1 == n {
-                Ok(42)
-            } else {
-                Err(())
-            }
-        })
-        .await;
-
-        result.map_err(|_| "Retry failed".to_string())
-    }
-
-    #[tokio::test]
-    async fn test_retry_success_after_retry() {
-        let result = retry(2, || retry_success_after_n_retries(2)).await;
-        assert_eq!(result, Ok(42));
     }
 }
