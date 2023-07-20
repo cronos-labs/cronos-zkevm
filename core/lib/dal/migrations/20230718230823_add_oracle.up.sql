6ce90528d8aecfe94c2a50204002c0842ed9ea46
jayt106 <jay.tseng@crypto.com>
2023-07-20 16:33:03 -0400

    fix oracle dal

--- core/lib/dal/migrations/20230718230823_add_oracle.up.sql
+++ core/lib/dal/migrations/20230718230823_add_oracle.up.sql
@@ -1,5 +1,6 @@
 -- Your SQL goes here
 CREATE TABLE oracle
 (
-    gas_token_adjust_coefficient NUMERIC NOT NULL PRIMARY KEY
+    gas_token_adjust_coefficient NUMERIC NOT NULL PRIMARY KEY,
+    gas_token_adjust_coefficient_updated_at TIMESTAMP
 );
--- core/lib/dal/src/oracle_dal.rs
+++ core/lib/dal/src/oracle_dal.rs
@@ -1,4 +1,8 @@
 use crate::StorageProcessor;
+use num::{rational::Ratio, BigUint};
+use zksync_utils::{big_decimal_to_ratio, ratio_to_big_decimal};
+
+pub(crate) const COEFFICIENT_PRECISION: usize = 10;
 
 #[derive(Debug)]
 pub struct OracleDal<'a, 'c> {
@@ -6,26 +10,23 @@ pub struct OracleDal<'a, 'c> {
 }
 
 impl OracleDal<'_, '_> {
-    pub fn update_adjust_coefficient(&mut self, adjust_coefficient: f64) {
-        async_std::task::block_on(async {
-            sqlx::query!(
-                "UPDATE oracle \
-                SET gas_token_adjust_coefficient = $1",
-                adjust_coefficient
-            )
-            .execute(self.storage.conn())
-            .await
-            .unwrap();
-        })
+    pub async fn update_adjust_coefficient(&mut self, adjust_coefficient: &Ratio<BigUint>) {
+        sqlx::query!(
+            "UPDATE oracle \
+            SET gas_token_adjust_coefficient = $1",
+            ratio_to_big_decimal(adjust_coefficient, COEFFICIENT_PRECISION)
+        )
+        .execute(self.storage.conn())
+        .await
+        .unwrap();
     }
 
-    pub fn get_adjust_coefficient(&mut self) -> f64 {
-        async_std::task::block_on(async {
-            let coef = sqlx::query!("SELECT gas_token_adjust_coefficient FROM oracle")()
-                .fetch_one(self.storage.conn())
-                .await
-                .unwrap();
-            coef.to_f64()
-        })
+    pub async fn get_adjust_coefficient(&mut self) -> Ratio<BigUint> {
+        let oracle = sqlx::query!("SELECT * FROM oracle")
+            .fetch_one(self.storage.conn())
+            .await
+            .unwrap();
+
+        big_decimal_to_ratio(&oracle.gas_token_adjust_coefficient).unwrap()
     }
 }
