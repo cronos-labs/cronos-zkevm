6d19322c90d9cc74f6f7a3f3eb024863b82fc027
jayt106 <jay.tseng@crypto.com>
2023-07-25 16:21:21 -0400

    revise oracle table contains

--- core/lib/dal/migrations/20230718230823_add_oracle.up.sql
+++ core/lib/dal/migrations/20230718230823_add_oracle.up.sql
@@ -1,6 +1,7 @@
 -- Your SQL goes here
 CREATE TABLE oracle
 (
-    gas_token_adjust_coefficient NUMERIC NOT NULL PRIMARY KEY,
-    gas_token_adjust_coefficient_updated_at TIMESTAMP
+    id SERIAL NOT NULL PRIMARY KEY ,
+    gas_token_adjust_coefficient NUMERIC NOT NULL DEFAULT 1.0,
+    gas_token_adjust_coefficient_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
